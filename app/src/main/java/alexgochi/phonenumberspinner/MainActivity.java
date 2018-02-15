4924b23fd5b2846584219aec2f4f09fc92c28617
Alex Gochi <alex.gochi.javan@gmail.com>
2018-02-16 00:21:43 +0700

    Initial commit

---
+++ app/src/main/java/alexgochi/phonenumberspinner/MainActivity.java
@@ -0,0 +1,68 @@
+package alexgochi.phonenumberspinner;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private String mSpinnerLabel = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.label_spinner);
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                                                R.array.labels_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
+    }
+
+    public void showText(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText_main);
+
+        if (editText != null) {
+            String showString = editText.getText().toString()+ " - " + mSpinnerLabel;
+            TextView textView = (TextView) findViewById(R.id.text_phonelabel);
+            textView.setText(showString);
+        }
+        if (mSpinnerLabel.equals("-")) {
+            assert editText != null;
+            String showString = editText.getText().toString();
+//          Toast.makeText(this, showString, Toast.LENGTH_SHORT).show();
+            TextView textView = (TextView) findViewById(R.id.text_phonelabel);
+            textView.setText(showString);
+        }
+        assert editText != null;
+        editText.getText().clear();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        mSpinnerLabel = adapterView.getItemAtPosition(i).toString();
+        showText(view);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        Log.d(TAG, "OnNothingSelected");
+    }
+}
